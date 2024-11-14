ebb1d0b86dd0be2f85fbab3251cd57888133ba5c
Flag10 <flag10@Flag10s-MacBook-Pro.local>
2024-11-14 08:38:04 +0300

    added

--- src/Array.java
+++ src/Array.java
@@ -1,30 +1,38 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author flag10
  */
 public class Array {
 
-    public static void displayNumbers() {
-//        String[] studentNames = {"clinton", "Joram", "Rose", "Vivian"};
-//        int[] marks = {80, 60, 70, 40};
-        int[]numbers={30,60,70,90,40,20,100};
-        int maximum=numbers[0];
+    public static void displayMaximumNumberInArray() {
+        int[] numbers = {30, 60, 70, 90, 40, 20, 100};
+        int maximum = numbers[0];
         int count;
-        for (count = 1; count<numbers.length; count++) {
-            if(numbers[count]>maximum){
-                maximum=numbers[count];
+        for (count = 1; count < numbers.length; count++) {
+            if (numbers[count] > maximum) {
+                maximum = numbers[count];
+                List array = new ArrayList();
+                array.add(maximum);
+                System.out.print("Maximum numbers is:" + array);
             }
-             
+
         }
-        System.out.println("Maximum number is:"+maximum);
+        System.out.println("========================================================");
+        List array = new ArrayList();
+        array.add(maximum);
+        System.out.println("Greatest number is:" + array);
+
     }
 
     public static void main(String[] args) {
-        displayNumbers();
+        displayMaximumNumberInArray();
     }
 }
